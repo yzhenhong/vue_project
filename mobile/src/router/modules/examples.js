22b523e18716cf051adee7e9dd3078a11cb7cd9a
yangzhenhong <736647835@qq.com>
2020-04-24 12:03:03 +0800

    添加title

--- mobile/src/router/modules/examples.js
+++ mobile/src/router/modules/examples.js
@@ -2,21 +2,33 @@ export default [
   {
     path: '/state',
     name: 'state',
+    meta: {
+      title: 'vuex',
+    },
     component: () => import('@/views/examples/state')
   },
   {
     path: '/swiper',
     name: 'swiper',
+    meta: {
+      title: 'swiper',
+    },
     component: () => import('@/views/examples/swiper'),
   },
   {
     path: '/upload',
     name: 'upload',
+    meta: {
+      title: 'upload',
+    },
     component: () => import('@/views/examples/upload'),
   },
   {
     path: '/pay',
     name: 'pay',
+    meta: {
+      title: 'pay',
+    },
     component: () => import('@/views/examples/pay'),
   },
 ]
