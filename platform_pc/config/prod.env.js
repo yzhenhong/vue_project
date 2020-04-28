e9e139d49a8ffeb08d0556b34318227945c9e3d2
yangzhenhong <736647835@qq.com>
2020-04-28 09:47:08 +0800

    删除文件

--- platform_pc/config/prod.env.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: JSON.stringify('production'),
-  // 上传图片地址
-  UPLOAD_URL: JSON.stringify('https://cos.xgeeklab.com')
-}
--- platform_pc/src/store/getters.js
+++
@@ -1,9 +0,0 @@
-const getters = {
-  userid: state => state.user.userid,
-  token: state => state.user.token,
-  menuList: state => state.user.menuList,
-  permissions: state => state.permission.permissions,
-  navs: state => state.nav.navs,
-  activeNavName: state => state.nav.activeNavName,
-};
-export default getters;
